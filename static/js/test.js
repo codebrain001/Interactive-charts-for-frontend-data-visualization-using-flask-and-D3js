b7b7fe664bdc564a26f6fec2e270da74721d576a
Brain Aboze <abozejohn@gmail.com>
2021-12-25 21:45:17 +0100

    updated base HTML

--- static/js/test.js
+++
@@ -1,8 +0,0 @@
- // const segment = d3.arc()
-    //     .outerRadius(outerRadius)
-    //     .innerRadius(0);
-
-    // const sections = d3.append("g")
-    //     .attr("transform", "translate(" + outerRadius + "," + outerRadius + ")")
-    //     .selectAll('path')
-    //     .data([dataset]); 
