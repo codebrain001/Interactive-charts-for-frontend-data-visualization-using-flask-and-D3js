e64a991f974a5fff80279aef5055c401c8f2c98f
Brain Aboze <abozejohn@gmail.com>
2021-12-27 13:53:33 +0100

    updated variable names

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, jsonify, render_template
-import csv
 import pandas as pd
 import numpy as np
 
--- static/js/index.js
+++ static/js/index.js
@@ -1,8 +1,8 @@
 const urls = [pieChartDataUrl, barChartDataUrl];
 
-Promise.all(urls.map(url => d3.json(url))).then(ready);
+Promise.all(urls.map(url => d3.json(url))).then(run);
 
-function ready(dataset) {
+function run(dataset) {
     d3PieChart(dataset[0], dataset[1]);
     d3BarChart(dataset[1]);
 };
